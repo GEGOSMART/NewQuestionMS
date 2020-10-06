5524758e665cc9f2fe343b0f6638dc21aa75a1ea
AndresFelipeCastellanos <afcastellanosn@unal.edu.co>
2020-10-06 17:34:06 -0500

    questionSchema

--- src/models/question.ts
+++ src/models/question.ts
@@ -1,18 +1,18 @@
 import {Schema, model} from 'mongoose'
 
 const Question = new Schema({
-    continent: { type: String },
-    questionCategory: { type: String, required: true },
-    questionImage: { type: String, required: true },
-    question: { type: String, required: true } ,
-    optionA: { type: String, required: true },
-    optionB: { type: String, required: true },
-    optionC: { type: String },
-    optionD: { type: String },
-    answer: { type: String, required: true },
-    created_ad: { type: Date , default: Date.now},
+    statement: { type: String, required: true },
+    image:    { type: String, required: true },
+    optionA:  { type: String, required: true },
+    optionB:  { type: String, required: true },
+    optionC:  { type: String },
+    optionD:  { type: String },
+    ans:      { type: String, required: true },
+    category:  { type: String, required: true },
+    continent:  { type: String },
+    created_at: { type: Date , default: Date.now},
     updated_at: { type: Date , default: Date.now},
-    created_by: { type: String , required: true}
+    creator: { type: String , required: true}
 });
 
 
